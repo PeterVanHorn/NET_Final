045452799507f43d8414ba871c271d09f1b3ed72
peter <petervanhornmt@gmail.com>
2024-04-12 14:15:46 -0600

    Fiddled with authentication

--- VanHorn_NET_Final/Models/Teacher.cs
+++ VanHorn_NET_Final/Models/Teacher.cs
@@ -5,6 +5,7 @@
         public int TeacherId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Password { get; set; }
         public virtual List<Quiz> Quizzes { get; set; }
         public virtual List<Student> Students { get; set; }
     }
