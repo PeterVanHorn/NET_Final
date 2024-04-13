e4971da0f52a81f6d4bb828a3fb4c4a9ed42e37f
Peter Vanhorn <petervanhornmt@gmail.com>
2024-04-13 08:56:49 -0600

    Started formatting questions for attempts

--- VanHorn_NET_Final/Pages/Quizzes/Attempt.cshtml.cs
+++ VanHorn_NET_Final/Pages/Quizzes/Attempt.cshtml.cs
@@ -1,12 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using VanHorn_NET_Final.Models;
+using VanHorn_NET_Final.Pages.Account;
 
 namespace VanHorn_NET_Final.Pages.Quizzes
 {
+    // 16 for this page layout
+    // 21 minutes for html layout
     public class AttemptModel : PageModel
     {
-        public void OnGet()
+        private readonly DomainContext _context;
+        public AttemptModel(DomainContext context)
         {
+            _context = context;
         }
+        public IEnumerable<Question> questionresults { get; set; }
+        public IEnumerable<Option> questionoptions { get; set; }
+        public async Task OnGet()
+        {
+            questionresults = await _context.Questions.ToListAsync();
+            questionoptions = await _context.Options.ToListAsync();
+        }
+
+        // <input id = "Radio1" type="radio" name="@item.QuestionId" class="rb" value="@item.Options[0]"/>
     }
 }
